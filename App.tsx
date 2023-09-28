6528e91a8b283bb2aab50c05eac8244d3a7ae74f
Himanshu Raj <himanshu.raj@hcl.com>
2023-09-28 17:23:27 +0530

    I have changed second time

--- App.tsx
+++ App.tsx
@@ -1,235 +1,132 @@
-/**
-
- * Sample React Native App
-
- * https://github.com/facebook/react-native
-
- *
-
- * @format
-
- */
-
- 
-
 import React from 'react';
 
-import type {PropsWithChildren} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-import {
+import Video from 'react-native-video';
 
-  SafeAreaView,
-
-  ScrollView,
-
-  StatusBar,
-
-  StyleSheet,
-
-  Text,
-
-  useColorScheme,
-
-  View,
-
-} from 'react-native';
-
- 
-
-import {
-
-  Colors,
-
-  DebugInstructions,
-
-  Header,
-
-  LearnMoreLinks,
-
-  ReloadInstructions,
-
-} from 'react-native/Libraries/NewAppScreen';
-
- 
-
-type SectionProps = PropsWithChildren<{
-
-  title: string;
-
-}>;
-
- 
-
-function Section({children, title}: SectionProps): JSX.Element {
-
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
 
   return (
 
-    <View style={styles.sectionContainer}>
+    <View style={styles.container}>
 
-      <Text
+      { /*Video */}
 
-        style={[
+      { <Video onLoad={(data)=>console.log(data)} source={{ uri: './assets/video.mp4' }} controls={true} /> }
 
-          styles.sectionTitle,
+      { /*Play Button*/ }
 
-          {
+      <TouchableOpacity style={styles.playButton}>
 
-            color: isDarkMode ? Colors.white : Colors.black,
+        <Text style={styles.playButtonText}>Play</Text>
 
-          },
+      </TouchableOpacity>
 
-        ]}>
+      {/* Video Details */}
 
-        {title}
+      <View style={styles.videoDetails}>
 
-      </Text>
+        <Text style={styles.videoTitle}></Text>
 
-      <Text
+        <Text style={styles.videoDescription}>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
 
-        style={[
+        <Text style={styles.uploadedByText}></Text>
 
-          styles.sectionDescription,
-
-          {
-
-            color: isDarkMode ? Colors.light : Colors.dark,
-
-          },
-
-        ]}>
-
-        {children}
-
-      </Text>
+      </View>
 
     </View>
 
   );
 
-}
+};
 
- 
 
-function App(): JSX.Element {
-
-  const isDarkMode = useColorScheme() === 'dark';
-
- 
-
-  const backgroundStyle = {
-
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-
-  };
-
- 
-
-  return (
-
-    <SafeAreaView style={backgroundStyle}>
-
-      <StatusBar
-
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-
-        backgroundColor={backgroundStyle.backgroundColor}
-
-      />
-
-      <ScrollView
-
-        contentInsetAdjustmentBehavior="automatic"
-
-        style={backgroundStyle}>
-
-        <Header />
-
-        <View
-
-          style={{
-
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-
-          }}>
-
-          <Section title="Step One">
-
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-
-            screen and then come back to see your edit.
-
-          </Section>
-
-          <Section title="See Your Changes">
-
-            <ReloadInstructions />
-
-          </Section>
-
-          <Section title="Debug">
-
-            <DebugInstructions />
-
-          </Section>
-
-          <Section title="Learn More">
-
-            Read the docs to discover what to do next:
-
-          </Section>
-
-          <LearnMoreLinks />
-
-        </View>
-
-      </ScrollView>
-
-    </SafeAreaView>
-
-  );
-
-}
-
- 
 
 const styles = StyleSheet.create({
 
-  sectionContainer: {
+  container: {
 
-    marginTop: 32,
+    flex: 1,
 
-    paddingHorizontal: 24,
+    backgroundColor: '#000',
+
+    justifyContent: 'center',
+
+    alignItems: 'center',
 
   },
 
-  sectionTitle: {
+  video: {
+
+    width: '100%',
+
+    height: '60%',
+
+  },
+
+  playButton: {
+
+    position: 'absolute',
+
+    backgroundColor: '#3498db',
+
+    padding: 15,
+
+    borderRadius: 50,
+
+    zIndex: 1,
+
+  },
+
+  playButtonText: {
+
+    color: '#fff',
+
+    fontWeight: 'bold',
+
+  },
+
+  videoDetails: {
+
+    width: '100%',
+
+    padding: 20,
+
+    backgroundColor: '#333',
+
+  },
+
+  videoTitle: {
+
+    color: '#fff',
 
     fontSize: 24,
 
-    fontWeight: '600',
+    fontWeight: 'bold',
 
   },
 
-  sectionDescription: {
+  videoDescription: {
 
-    marginTop: 8,
+    color: '#fff',
 
-    fontSize: 18,
+    width: 360,
 
-    fontWeight: '400',
+    height: 357,
+
+    top: 443,
+
 
   },
 
-  highlight: {
+  uploadedByText: {
 
-    fontWeight: '700',
+    color: '#fff',
 
   },
 
 });
 
- 
+
 
 export default App;
